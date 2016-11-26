88851d60e49a35c619e72a60cefc4418e42180af
marcia.villalba <marcia.villalba@gmail.com>
2016-11-26 17:15:59 +0200

    fixing old lamdba with server less 1.x changes

--- handler.js
+++ handler.js
@@ -1,9 +1,13 @@
 'use strict';
 
-var meetupService = require('./service/index');
+const meetupService = require('./service/index');
 
 module.exports.getMeetupEventsCalendar = function(event, context, callback) {
-  var calendarEventId = event.path.id;
+  const calendarEventId = event.pathParameters.id;
   meetupService.getIcalEvents(calendarEventId);
-  callback(null, "Calendar Created for " + calendarEventId + "!");
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ "message": 'Calendar Created for ' + calendarEventId + '!'})
+  };
+  callback(null, response);
 }
